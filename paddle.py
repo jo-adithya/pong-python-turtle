84de7ec54ae8172e6ee648f4e62b33dba6767ea2
Jonathan Adithya <jo.adithya391@gmail.com>
2021-05-05 15:46:20 +0700

    modify paddle class

--- paddle.py
+++ paddle.py
@@ -5,10 +5,11 @@ class Paddle(Turtle):
     def __init__(self, pos):
         super().__init__()
         self.shape('square')
-        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.shapesize(stretch_wid=1, stretch_len=5)
         self.color('white')
         self.penup()
         self.setpos(pos)
+        self.setheading(90)
 
     def up(self):
         self.forward(20)
